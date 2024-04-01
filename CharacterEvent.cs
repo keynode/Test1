74b78a76aa62e994cd8b1205c072f3b25b262cad
Timur Davidenko <keynode@gmail.com>
2024-04-01 16:28:19 +0300

    Add files via upload

---
+++ CharacterEvent.cs
@@ -0,0 +1,11 @@
+﻿using G01.AzurePlayfabCommon.Models.PlayFabContext;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("UnitTest")]
+namespace G01.GameServices.PlayStreamEvents.Events
+{
+	internal record CharacterEvent(EventTypes eventType, CharacterId CharacterId)
+		: BaseGameEvent(eventType)
+	{
+	}
+}
